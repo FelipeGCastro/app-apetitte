e263e44ea5bff4b9009571d969614e88ca2dea14
Luiz Felipe Castro <luiz@Kenetto-MBP-13.local>
2019-12-04 03:04:20 +0000

    depurando código

--- src/store/ducks/days.js
+++ src/store/ducks/days.js
@@ -30,73 +30,25 @@ export const INITIAL_STATE = Immutable({
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.LOAD_DATA]: (state, { data }) => state.merge({ days: data }),
   [Types.UPDATE_PRODUCT]: (state, { dayId, productIndex, product }) => {
-    if (productIndex === 'snack1') {
-      return state.merge({
-        days: [
-          ...state.days.filter((day) => day.id !== dayId),
-          {
-            ...state.days.filter((day) => day.id === dayId)[0],
-            snack1: product
-          }
-        ].sort((a, b) => (a.id > b.id) ? 1 : -1)
-      })
-    }
-    if (productIndex === 'snack2') {
-      return state.merge({
-        days: [
-          ...state.days.filter((day) => day.id !== dayId),
-          {
-            ...state.days.filter((day) => day.id === dayId)[0],
-            snack2: product
-          }
-        ].sort((a, b) => (a.id > b.id) ? 1 : -1)
-      })
-    }
-    if (productIndex === 'drink') {
-      return state.merge({
-        days: [
-          ...state.days.filter((day) => day.id !== dayId),
-          {
-            ...state.days.filter((day) => day.id === dayId)[0],
-            drink: product
-          }
-        ].sort((a, b) => (a.id > b.id) ? 1 : -1)
-      })
-    }
+    return state.merge({
+      days: [
+        ...state.days.filter((day) => day.id !== dayId),
+        {
+          ...state.days.filter((day) => day.id === dayId)[0],
+          [productIndex]: product
+        }
+      ].sort((a, b) => (a.id > b.id) ? 1 : -1)
+    })
   },
   [Types.REMOVE_PRODUCT]: (state, { dayId, productIndex }) => {
-    if (productIndex === 'snack1') {
-      return state.merge({
-        days: [
-          ...state.days.filter((day) => day.id !== dayId),
-          {
-            ...state.days.filter((day) => day.id === dayId)[0],
-            snack1: null
-          }
-        ].sort((a, b) => (a.id > b.id) ? 1 : -1)
-      })
-    }
-    if (productIndex === 'snack2') {
-      return state.merge({
-        days: [
-          ...state.days.filter((day) => day.id !== dayId),
-          {
-            ...state.days.filter((day) => day.id === dayId)[0],
-            snack2: null
-          }
-        ].sort((a, b) => (a.id > b.id) ? 1 : -1)
-      })
-    }
-    if (productIndex === 'drink') {
-      return state.merge({
-        days: [
-          ...state.days.filter((day) => day.id !== dayId),
-          {
-            ...state.days.filter((day) => day.id === dayId)[0],
-            drink: null
-          }
-        ].sort((a, b) => (a.id > b.id) ? 1 : -1)
-      })
-    }
+    return state.merge({
+      days: [
+        ...state.days.filter((day) => day.id !== dayId),
+        {
+          ...state.days.filter((day) => day.id === dayId)[0],
+          [productIndex]: null
+        }
+      ].sort((a, b) => (a.id > b.id) ? 1 : -1)
+    })
   }
 })
